2bd0dc61e3347bee971b674a08cbe9ff09c15e78
Aswathy k.r <aswathy19kr@gmail.com>
2024-11-22 23:31:14 +0530

    added router for deleting a post

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -67,16 +67,29 @@ router.put('/:id', async (req, res) => {
 
 // DELETE POST
 
-router.get('/:id',async(req,res)=>{
-    try{
-        const user = await User.findById(req.params.id);
-        const {password,...others} = user._doc
-        res.status(200).json(others)
-    }catch(err){
+router.delete('/:id', async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        if (!post) {
+            return res.status(404).json({ message: "Post not found!" });
+        }
+        if (post.username === req.body.username) {
+            try {
+                await Post.findByIdAndDelete(
+                    req.params.id
+                );
+                res.status(200).json("Your Post has been deleted...");
+            } catch (err) {
+                res.status(500).json({ message: "Internal Server Error", error: err });
+            }
+        } else {
+            res.status(401).json({ message: "You can only delete your own post!" });
+        }
+    } catch (err) {
         console.error("Error:", err);
-        res.status(404).json("User not found!");
+        res.status(500).json({ message: "Internal Server Error", error: err });
     }
-})
+});
 
 
 
